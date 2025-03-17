19b17477353153541bd7b617183399470570a31f
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-17 11:25:38 +0000

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Kyaw Thant Zin(George) on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
