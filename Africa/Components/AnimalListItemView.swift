d7da1fa47e41445dccd3255bfb2c806ca57aa748
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-18 12:10:17 +0000

    Implementing animal list view to the content view

---
+++ Africa/Components/AnimalListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalListItemView.swift
+//  Africa
+//
+//  Created by Kyaw Thant Zin(George) on 3/18/25.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalListItemView()
+}
---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Kyaw Thant Zin(George) on 3/18/25.
+//
+
+import Foundation
