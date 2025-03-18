dda339a8198cef1f4c6486f2915fb1458b8ad759
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-18 11:06:36 +0000

    Adding Tabs on the main view

--- Africa/App/MainView.swift
+++ Africa/App/MainView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var selectedTab: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selectedTab){
+            Tab("Browse",systemImage: "square.grid.2x2",value: 0){
+                ContentView()
+            }
+            Tab("Watch",systemImage: "play.rectangle",value: 1){
+                VideoListView()
+            }
+            Tab("Location",systemImage: "map",value: 2){
+                MapView()
+            }
+            Tab("Gallery", systemImage: "photo",value: 3){
+                GalleryView()
+            }
+                
+        }//: Tab View
     }
 }
 
