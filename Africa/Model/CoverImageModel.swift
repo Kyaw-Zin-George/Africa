5b147df0db1e36f1bec7f42047d37f24e4ae6dc9
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-18 11:44:59 +0000

    Creating cover image model for decoding

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Kyaw Thant Zin(George) on 3/18/25.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable{
+    let id: Int
+    let name: String
+}
