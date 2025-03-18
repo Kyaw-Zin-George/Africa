d2f3f04ff536653b83143eb41129e42ed9ec382f
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-18 11:36:51 +0000

    Create cover image view

--- Africa/Components/CoverImageView.swift
+++ Africa/Components/CoverImageView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct CoverImageView: View {
+    
+    //MARK: PROPERTIES
+    
+    //MARK: BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                    .scaledToFit()
+            }//Loop
+        }//Tab
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
+//MARK: PREVIEW
 #Preview {
     CoverImageView()
 }
