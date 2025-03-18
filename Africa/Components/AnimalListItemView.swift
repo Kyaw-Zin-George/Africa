e9bff7ef262f6ed1f8c49b9db4bd6b52b76a11c9
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-18 12:11:50 +0000

    Creating reusable animal list view

--- Africa/Components/AnimalListItemView.swift
+++ Africa/Components/AnimalListItemView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct AnimalListItemView: View {
+    let animal: Animal
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center, spacing: 16){
+            Image(animal.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 90, height: 90)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            VStack(alignment: .leading, spacing: 8){
+                Text(animal.name)
+                    .font(.title2.weight(.heavy))
+                    .foregroundStyle(.accent)
+                Text(animal.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding(.trailing, 8)
+            }
+        }
     }
 }
 
 #Preview {
-    AnimalListItemView()
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    AnimalListItemView(animal: animals[1])
+        .padding()
 }
