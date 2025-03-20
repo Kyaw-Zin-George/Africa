f46ff68edd221bbec81c09f5df1f479b821e1dc9
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-20 14:31:56 +0000

    Creating video play view with logo overlay

--- Africa/Components/VideoPlayerView.swift
+++ Africa/Components/VideoPlayerView.swift
@@ -6,13 +6,34 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct VideoPlayerView: View {
+    var videoSelected: String
+    var videoTitle: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VideoPlayer(player: playvideo(fileName: videoSelected, fileFormat: "mp4")){
+                //Text(videoTitle)
+                
+            }.overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top,6)
+                    .padding(.horizontal,8)
+                ,alignment: .topLeading
+            )
+        }
+        .tint(.accent)
+        .navigationTitle(videoTitle)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    VideoPlayerView()
+    NavigationView {
+        VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+    }
 }
