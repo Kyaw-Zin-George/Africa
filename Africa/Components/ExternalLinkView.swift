ee9d1b87f1f1e4eeaf5dcc9d0a95a2a92721534f
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-20 06:54:05 +0000

    Creating link view for users to learn more about an animal

--- Africa/Components/ExternalLinkView.swift
+++ Africa/Components/ExternalLinkView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct ExternalLinkView: View {
+    let animal: Animal
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox{
+            HStack{
+                Image(systemName: "globe")
+                Text("Wikepedia")
+                Spacer()
+                
+                Group{
+                    Image(systemName: "arrow.up.right.square")
+                    
+                    Link(animal.name, destination: (URL(string: animal.link) ?? URL(string: "https://www.wikepedia.org"))!)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ExternalLinkView()
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    ExternalLinkView(animal: animals[0])
 }
