01fd09be876e05d6c20dab00e884fdc4182e07ce
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-20 14:00:41 +0000

    creating Video List view

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct VideoListView: View {
+    @State private var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List{
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical,10)
+                }//loop
+            }//list
+            .listStyle(InsetListStyle())
+            .navigationBarTitle("Videos",displayMode: .inline)
+            .toolbar{
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button{
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }label: {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
+    
     VideoListView()
 }
