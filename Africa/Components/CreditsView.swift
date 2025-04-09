5510d77bcbf5e12d244551128c91be9e42f657be
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-09 16:09:29 +0100

    Create credits and copyright view

--- Africa/Components/CreditsView.swift
+++ Africa/Components/CreditsView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct CreditsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+   
+        VStack {
+            Image("compass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 128, height: 128)
+            Text("""
+    Copyright © Kyaw Zin
+    All right reserved
+    Better Apps ❤️ Less Code
+    """)
+            .font(.footnote)
+            .multilineTextAlignment(.center)
+        }//vstack
+        .padding()
+        .opacity(0.4)
     }
 }
 
